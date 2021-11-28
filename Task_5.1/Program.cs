9d17fb5d78f56dc272d3ef1d69ef3964b10be316
ander7even <ander7even@mail.ru>
2021-11-28 18:30:25 +0900

    Добавьте файлы проекта.

---
+++ Task_5.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)          //метод нахождения среднего арифметического массива
+        {
+
+            Console.WriteLine("Введите семь чисел массива массива");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            int a4 = Convert.ToInt32(Console.ReadLine());
+            int a5 = Convert.ToInt32(Console.ReadLine());
+            int a6 = Convert.ToInt32(Console.ReadLine());
+            int a7 = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] m = new int[7] { a1, a2, a3, a4, a5, a6, a7 };
+
+            double summa = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                summa += m[i];
+            }
+
+            Console.Write("Среднее арифметическое значение равно={0:f2} ", summa / 7);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
